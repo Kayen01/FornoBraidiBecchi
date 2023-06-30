5df93a0a6ea2e765f3fd5c41322eff9c24c0ee74
Kayen01 <118180331+Kayen01@users.noreply.github.com>
2023-06-30 11:38:08 +0200

    Update graph_maker.py

--- graph_maker.py
+++ graph_maker.py
@@ -3,23 +3,26 @@ import os
 import numpy as np
 
 mod = "PID"         #valori possibili OnOff oppure PID
-V_onoff = 5
-Kp = 0.1
-Ki = 0.011
-Kd = 0.07
+V_onoff = 0        #settare tutti i valori relativi all' esperimento di cui si vuole fare il grafico
+Kp = 0
+Ki = 0
+Kd = 0
 t_setpoint = 80
-num = 22
+num = 22                #numero dell' esperimento (serve per )
 temperatura = []
 tempo = []
-colore_grafico = "brown"
-directory_dati = r"C:\\Users\\feder\\OneDrive\\Desktop\\Roba\\Scuola\\Università\\Elettronica e Acquisiszione Dati\\Dati"
+colore_grafico = ""    #colore del grafico
+directory_dati = r""    #inserire la propria directory dove vengono posti i dati ricavati dagli esperimenti
+percorso_file_descrizioni = r""  #inserire il percorso al file di descrizioni
+directory_grafici = r""    #inserire percorso della directory dei grafici
 
 for file in os.listdir(directory_dati):
     if(file == "temperatura.txt"):
         with open(os.path.join(directory_dati,"temperatura.txt"),'r') as file:
             for riga in file:
-                temperatura.append(float(riga[:len(riga)-1]))
-        os.rename(os.path.join(directory_dati,"temperatura.txt"),os.path.join(directory_dati,"temperatura_"+str(num)+".txt"))
+                temperatura.append(float(riga[:len(riga)-1]))    #toglie gli \n alla fine della riga
+        os.rename(os.path.join(directory_dati,"temperatura.txt"),os.path.join(directory_dati,"temperatura_"+str(num)+".txt"))    #rinomina il file temperatura (quello uscito dall' esperimento)
+                                                                                                                                #in uno contraddistinto da un numero legato allo specifico esperimento
     if(file == "tempo.txt"):
         with open(os.path.join(directory_dati,"tempo.txt"),'r') as file:
             for riga in file:
@@ -39,6 +42,6 @@ plt.annotate("Parametri:\n\n" + annotazione, xy=(max(tempo)-30, 100), xycoords='
              xytext=(-10, -10), textcoords='offset points',
              ha='left', va='top',bbox=dict(boxstyle='square', fc='white', ec='black'))
 annotazione = annotazione.replace("\n","\t")
-with open(r"C:\\Users\\feder\\OneDrive\\Desktop\\Roba\\Scuola\\Università\\Elettronica e Acquisiszione Dati\\Descrizioni.txt", "a") as file:
+with open(percorso_file_descrizioni, "a") as file:
     file.write(str(num) + ") " + mod + "\t" + annotazione + "\n")
-plt.savefig(r"C:\\Users\\feder\\OneDrive\\Desktop\\Roba\\Scuola\\Università\\Elettronica e Acquisiszione Dati\\Grafici\\graph_{}".format(num))
+plt.savefig(os.path.join(directory_grafici,"graph_{}".format(num)))
